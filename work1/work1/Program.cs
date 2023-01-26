e1b447cc688f5fc65e860ae51d87984592f59a0d
unknown <shamsshsh@code.edu.az>
2023-01-26 23:07:34 +0400

    26.01.23

---
+++ work1/work1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace work1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int loopCount = 0;
+            int num = 11;
+
+            bool isPrime=false;
+            for(var i=2;i*i<num;i++)
+            {
+                loopCount++;
+                if(num%i==0)
+                    {
+                    isPrime = true;
+                    break;
+                    }
+            }
+            if (isPrime == true)
+                Console.WriteLine("murekkebdir");
+            else
+                Console.WriteLine("Sadedir");
+            Console.WriteLine(loopCount);
+
+            int number = -2;
+            if(number%2==0)
+            {
+                Console.WriteLine("musbet eded");
+            }
+            else if (number < 0)
+            {
+                Console.WriteLine("menfidir");
+
+            }
+            else
+            {
+                Console.WriteLine("sifirdir");
+            }
+        }
+    }
+}
